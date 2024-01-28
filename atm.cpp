c0c17f6d62dc2dacedcf39db6f2040d5570fc701
AichaLAMMAMRA <aicha.lammamra07@gmail.com>
2024-01-28 19:04:05 +0100

    1st version

---
+++ atm.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+/*
+  1. Check Balance
+  2. 
+  3.
+  4.
+*/
+
+class CustomerInfos : public ATM{
+
+
+public:
+	void setAccountNB (ATM m_objAtm){ 
+
+		
+
+	
+		
+};
+
+class ATM {
+
+protected:
+	long int m_accountNB;
+	string m_name;
+	string m_mobilNb;
+	double m_balance;
+	int m_PIN;
+
+
+public:
+	ATM (long int accountNB, string name, string mobilNb,double balance,int PIN){
+		m_accountNB = accountNB;
+		m_name = name;
+		m_mobilNb = mobilNb;
+		m_balance = balance;
+		m_PIN = PIN;
+	}
+
+
+};
+
+
+
+int main (){
+
+	retour 0;
+}
+
+/*
+}
+	string setName (ATM m_objAtm){
+		
+	
+
+	}
+	string setMobilNb (ATM m_objAtm){
+
+	
+
+	}
+	double setBalance (ATM m_objAtm){ 
+		
+		
+
+	}
+
+	int setPIN (ATM m_objAtm){
+
+
+	}
+	*/
